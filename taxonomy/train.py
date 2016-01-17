741da6527993ef83a3c2df839e30d91299c2043e
John Co-Reyes <jdcoreyes@gmail.com>
2016-01-16 19:06:23 -0800

    Update docs

--- taxonomy/train.py
+++ taxonomy/train.py
@@ -1,22 +1,7 @@
-#!/usr/bin/env python
-# ----------------------------------------------------------------------------
-# Copyright 2015 Nervana Systems Inc.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ----------------------------------------------------------------------------
 """
-Runs one epoch of Alexnet on imagenet data.
-For running complete alexnet
-alexnet.py -e 90 -eval 1 -s <save-path> -w <path-to-saved-batches>
+Trains various models.
+Example command:
+python train.py --model_type alexnet -s ~/taxonomic-training-saved-models -w ~/nervana/data/NABirds_batchs -b gpu
 """
 
 from neon.util.argparser import NeonArgparser
@@ -30,10 +15,7 @@ from neon.callbacks.callbacks import Callbacks
 
 import models
 
-"""
-Example command:
-python train.py --model_type alexnet -s ~/taxonomic-training-saved-models -w ~/nervana/data/NABirds_batchs -b gpu
-"""
+
 
 # parse the command line arguments (generates the backend)
 parser = NeonArgparser(__doc__)
